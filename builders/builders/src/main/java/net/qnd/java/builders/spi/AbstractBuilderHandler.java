0f61e384ad196c9c84f5fdd10e9ae3a8ea505a24
droy314 <deepu_roy@yahoo.com>
2014-01-07 22:25:16 +0530

    Formatted

--- builders/builders/src/main/java/net/qnd/java/builders/spi/AbstractBuilderHandler.java
+++ builders/builders/src/main/java/net/qnd/java/builders/spi/AbstractBuilderHandler.java
@@ -15,8 +15,9 @@ public abstract class AbstractBuilderHandler {
 	private final List<Command> commands = new ArrayList<Command>();
 	protected final Object parentBuilder;
 	protected final BuilderFactory factory;
-	
-	protected AbstractBuilderHandler(BuilderFactory factory,Class<?> builderType, Object parentBuilder) {
+
+	protected AbstractBuilderHandler(BuilderFactory factory,
+			Class<?> builderType, Object parentBuilder) {
 		this.factory = factory;
 		this.productType = BuilderUtils.getBuiltProductType(builderType);
 		this.parentBuilder = parentBuilder;
@@ -25,33 +26,36 @@ public abstract class AbstractBuilderHandler {
 
 	protected Object stubCall(Object proxy, Method method, Object[] args)
 			throws Throwable, IllegalAccessException, InvocationTargetException {
-				String methodName = method.getName();
-				final int paramLength = method.getParameterTypes().length;
-				if ("end".equals(methodName)) {
-					return parentBuilder;
-				} else if ("build".equals(methodName)) {
-					return build();
-				} else if ("toString".equals(methodName) || "equals".equals(methodName)
-						|| "hashCode".equals(methodName)) {
-					return method.invoke(proxy, args);
-				} /*else if (methodName.startsWith("add") && paramLength == 0) {
-					// Returns a builder for a child
-				} */else if (paramLength == 1) {
-					final Class<?> propertyType = method.getParameterTypes()[0];
-					final Object value = args[0];
-					Command cmd = Commands.setPropertyValue(methodName, propertyType, value);
-					addCommand(cmd);
-					return proxy;
-				} else if (paramLength == 0) {
-					// FIXME: What happens if the return type is not a builder?
-					Builder<?> nestedBuilder = (Builder<?>) factory
-							.createNestedBuilder(method.getReturnType(), proxy);
-					Command cmd = Commands.setPropertyWithBuilderOutput(methodName, nestedBuilder);
-					addCommand(cmd);
-					return nestedBuilder;
-				}
-				return proxy;
-			}
+		String methodName = method.getName();
+		final int paramLength = method.getParameterTypes().length;
+		if ("end".equals(methodName)) {
+			return parentBuilder;
+		} else if ("build".equals(methodName)) {
+			return build();
+		} else if ("toString".equals(methodName) || "equals".equals(methodName)
+				|| "hashCode".equals(methodName)) {
+			return method.invoke(proxy, args);
+		} /*
+		 * else if (methodName.startsWith("add") && paramLength == 0) { //
+		 * Returns a builder for a child }
+		 */else if (paramLength == 1) {
+			final Class<?> propertyType = method.getParameterTypes()[0];
+			final Object value = args[0];
+			Command cmd = Commands.setPropertyValue(methodName, propertyType,
+					value);
+			addCommand(cmd);
+			return proxy;
+		} else if (paramLength == 0) {
+			// FIXME: What happens if the return type is not a builder?
+			Builder<?> nestedBuilder = (Builder<?>) factory
+					.createNestedBuilder(method.getReturnType(), proxy);
+			Command cmd = Commands.setPropertyWithBuilderOutput(methodName,
+					nestedBuilder);
+			addCommand(cmd);
+			return nestedBuilder;
+		}
+		return proxy;
+	}
 
 	protected void addCommand(Command command) {
 		if (command != null) {
