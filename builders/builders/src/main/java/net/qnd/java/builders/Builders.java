7ad8beb1238ba7f8ac936af395a7306a8bc7ee75
droy314 <deepu_roy@yahoo.com>
2014-01-07 22:24:24 +0530

    Formatted

--- builders/builders/src/main/java/net/qnd/java/builders/Builders.java
+++ builders/builders/src/main/java/net/qnd/java/builders/Builders.java
@@ -3,16 +3,15 @@ package net.qnd.java.builders;
 import net.qnd.java.builders.reflect.ReflectProxyBuilderFactory;
 import net.qnd.java.builders.spi.BuilderFactory;
 
-
 public class Builders {
 
 	public static <BuilderT> BuilderT newBuilder(Class<BuilderT> builderType) {
 		return newBuilder(new ReflectProxyBuilderFactory(), builderType);
 	}
-	
-	public static <BuilderT> BuilderT newBuilder(BuilderFactory factory, Class<BuilderT> builderType) {
+
+	public static <BuilderT> BuilderT newBuilder(BuilderFactory factory,
+			Class<BuilderT> builderType) {
 		return factory.createBuilder(builderType);
 	}
 
-	
 }
